f17b5fb29478fc39d1c8374615558574f1541476
Fabrice Normandin <normandf@mila.quebec>
2025-03-03 14:37:27 -0500

    Disable jax GPU memory pre-allocation
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- uv_job.sh
+++ uv_job.sh
@@ -13,4 +13,7 @@ export HF_DATASETS_OFFLINE=1
 export HF_HOME=$SCRATCH/huggingface
 export HF_HUB_OFFLINE=1
 
+# Disable gpu mem pre-allocation so we can see how much we're using in wandb.
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
+
 srun uv run --offline --frozen --all-extras "$@"
